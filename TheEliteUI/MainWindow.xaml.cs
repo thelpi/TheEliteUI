2092fdfc6988be07dceb6b8cffeb23ba01ffefe5
Lpi <thelpi.v3@gmail.com>
2021-04-18 21:15:39 +0200

    Dynamic ranking fisrt atttempt

--- TheEliteUI/MainWindow.xaml.cs
+++ TheEliteUI/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Windows;
+using TheEliteUI.Model;
 
 namespace TheEliteUI
 {
@@ -9,7 +12,17 @@ namespace TheEliteUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int TimerDelay = 1000;
+        private const Game SelectedGame = Game.GoldenEye;
+
         private readonly IBackProvider _provider;
+        private readonly Timer _timer;
+
+        private readonly IReadOnlyDictionary<Game, DateTime> _rankingStart = new Dictionary<Game, DateTime>
+        {
+            { Game.GoldenEye, new DateTime(1998, 07, 26) },
+            { Game.PerfectDark, new DateTime(2000, 01, 01) }
+        };
 
         public MainWindow()
             : this(new BackProvider())
@@ -20,19 +33,57 @@ namespace TheEliteUI
             _provider = provider ?? throw new ArgumentNullException(nameof(provider));
             InitializeComponent();
             RankingDatePicker.SelectedDateChanged += RankingDatePicker_SelectedDateChanged;
-            RankingDatePicker.SelectedDate = DateTime.Now.Date;
+            RankingDatePicker.SelectedDate = _rankingStart[SelectedGame];
+            _timer = new Timer(TimerDelay);
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                var currentDate = RankingDatePicker.SelectedDate.Value;
+
+                if (currentDate >= DateTime.Today)
+                {
+                    AnimationButton_Click(null, null);
+                    return;
+                }
+
+                currentDate = currentDate.AddDays(7);
+                if (currentDate > DateTime.Today)
+                {
+                    currentDate = DateTime.Today;
+                }
+
+                RankingDatePicker.SelectedDate = currentDate;
+            });
         }
 
         private void RankingDatePicker_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             var itemsSource = _provider
-                .GetRankingAsync(Model.Game.GoldenEye, RankingDatePicker.SelectedDate.Value)
+                .GetRankingAsync(SelectedGame, RankingDatePicker.SelectedDate.Value)
                 .GetAwaiter()
                 .GetResult();
 
             RankingView.ItemsSource = itemsSource
                 .Select((_ , r) => new ViewModel.RankingViewModel(_, r + 1))
                 .ToList();
+        }
+
+        private void AnimationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_timer.Enabled)
+            {
+                _timer.Stop();
+                AnimationButton.Content = "Start Animation";
+            }
+            else
+            {
+                _timer.Start();
+                AnimationButton.Content = "Stop Animation";
+            }
         }
     }
 }
