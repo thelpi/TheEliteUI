c885749f73b205d69bdd1412d1c58ca1d759194c
Lpi <thelpi.v3@gmail.com>
2021-04-27 21:49:01 +0200

    Replace code behind by real binding

--- TheEliteUI/Dtos/PlayerRankingDto.cs
+++ TheEliteUI/Dtos/PlayerRankingDto.cs
@@ -27,11 +27,53 @@ namespace TheEliteUI.Dtos
         public string PlayerColor { get; set; }
         public int Rank { get; set; }
         public int SubRank { get; set; }
-        public IReadOnlyDictionary<Level, int> LevelPoints { get; set; }
-        public IReadOnlyDictionary<Level, int> LevelUntiedRecordsCount { get; set; }
-        public IReadOnlyDictionary<Level, int> LevelRecordsCount { get; set; }
-        public IReadOnlyDictionary<Level, int> LevelCumuledTime { get; set; }
+        public IReadOnlyDictionary<Level, int> LevelPoints { private get; set; }
+        public IReadOnlyDictionary<Level, int> LevelUntiedRecordsCount { private get; set; }
+        public IReadOnlyDictionary<Level, int> LevelRecordsCount { private get; set; }
+        public IReadOnlyDictionary<Level, int> LevelCumuledTime { private get; set; }
         public IReadOnlyDictionary<Stage, IReadOnlyDictionary<Level, StageLevelDetails>> Details { get; set; }
+
+        private List<LevelDetails> _levels = null;
+
+        public IReadOnlyCollection<LevelDetails> Levels
+        {
+            get
+            {
+                if (_levels == null)
+                {
+                    _levels = new List<LevelDetails>
+                    {
+                        new LevelDetails(Level.Easy, this),
+                        new LevelDetails(Level.Medium, this),
+                        new LevelDetails(Level.Hard, this)
+                    };
+                }
+                return _levels;
+            }
+        }
+
+        public class LevelDetails
+        {
+            public Level Level { get; }
+            public int? Points { get; }
+            public int? WrTiedCount { get; }
+            public int? WrUntiedCount { get; }
+            public int? Time { get; }
+
+            internal LevelDetails(Level level, PlayerRankingDto dto)
+            {
+                Level = level;
+                Points = GetOrDefault(level, dto.LevelPoints);
+                Time = GetOrDefault(level, dto.LevelCumuledTime);
+                WrTiedCount = GetOrDefault(level, dto.LevelRecordsCount);
+                WrUntiedCount = GetOrDefault(level, dto.LevelUntiedRecordsCount);
+            }
+
+            private static int? GetOrDefault(Level level, IReadOnlyDictionary<Level, int> dict)
+            {
+                return dict.ContainsKey(level) ? dict[level] : default(int?);
+            }
+        }
 
         public class StageLevelDetails
         {
--- TheEliteUI/PlayerRankingToolTipControl.xaml.cs
+++ TheEliteUI/PlayerRankingToolTipControl.xaml.cs
@@ -17,34 +17,7 @@ namespace TheEliteUI
         public PlayerRankingToolTipControl(PlayerRankingDto dto)
         {
             InitializeComponent();
-            DataContext = dto;
-            // TODO: sets real binding instead of code-behind
-            SetLevelLabels(dto, Level.Easy);
-            SetLevelLabels(dto, Level.Medium);
-            SetLevelLabels(dto, Level.Hard);
-        }
-
-        private void SetLevelLabels(PlayerRankingDto dto, Level level)
-        {
-            (FindName($"{level}Label") as Label).Content = level;
-            (FindName($"{level}PointsLabel") as Label).Content =
-                dto.LevelPoints.ContainsKey(level)
-                    ? dto.LevelPoints[level].ToString()
-                    : NA;
-            (FindName($"{level}TimeLabel") as Label).Content =
-                dto.LevelCumuledTime.ContainsKey(level)
-                    ? _timeToTextConverter.InnerConvert(dto.LevelCumuledTime[level]).ToString()
-                    : NA;
-            (FindName($"{level}WrLabel") as Label).Content =
-                dto.LevelRecordsCount.ContainsKey(level)
-                    ? string.Concat(
-                        dto.LevelRecordsCount[level],
-                        " (",
-                        (dto.LevelUntiedRecordsCount.ContainsKey(level)
-                            ? dto.LevelUntiedRecordsCount[level].ToString()
-                            : NA),
-                        ")")
-                    : NA;
+            LevelsView.ItemsSource = dto.Levels;
         }
     }
 }
